ab150815f08e2b096c6a23a79a312742fd1cbbaa
hotoku <horikoshi.et.al@gmail.com>
2019-12-29 11:45:28 +0900

    rstudio.sh

--- rstudio.sh
+++ rstudio.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+TERMINATE=?
+while getopts t OPT; do
+    case $OPT in
+        t) TERMINATE=true         ;;
+        *) print_help; exit 1     ;;
+    esac
+done
+
+if [ $TERMINATE = true ]; then
+    docker stop rstudio
+    exit 0
+fi
+
 open http://localhost:9001
